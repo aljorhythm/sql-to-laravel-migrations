4b1bf73353acb3c3dfb7a21fb4156c54fa6030fc
Joel Lim <103879u@gmail.com>
2020-06-13 15:26:16 +0800

    autopep

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 python3 -m pip install --upgrade autopep8
-autopep8 --in-place --aggressive --recursive .
+autopep8 --in-place --aggressive --recursive tests/ sql_to_laravel_migrations/
 python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
