96300613c5c0c336fb246a982ed40f0811ece88d
Joel Lim <103879u@gmail.com>
2020-06-13 12:56:21 +0800

    lint

--- package.sh
+++ package.sh
@@ -1,2 +1,4 @@
+python3 -m pip install --upgrade autopep8
+autopep8 --in-place --aggressive --recursive .
 python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
